973ff075460288625b54d7a4be171637b1852128
Tim Frazier <tim.frazier@gmail.com>
2011-04-07 15:12:24 -0500

    Query Builder can now pretty much build any query, now i need to implement it throughout P3

--- lib/P3/Database/Query/Builder.php
+++ lib/P3/Database/Query/Builder.php
@@ -26,9 +26,10 @@ class Builder
 	private $_table     = null;
 
 //- Public
-	public function __construct($table_or_model)
+	public function __construct($table_or_model, $alias = null)
 	{
-		$this->_table = is_string($table_or_model) ? $table_or_model : $table_or_model::table();
+		$this->_alias = $alias;
+		$this->_table = (is_string($table_or_model) ? $table_or_model : $table_or_model::table()).(is_null($alias) ? '' : ' '.$alias);
 	}
 
 	public function delete()
@@ -45,55 +46,95 @@ class Builder
 		var_dump($query); die;
 	}
 
-	public function group()
+	public function group($fields, $mode = self::MODE_OVERRIDE)
 	{
+		$fields = is_array($fields) ? implode(', ', $fields) : $fields;
+
+		$this->_section('group', $fields, $mode);
 		return $this;
 	}
 
-	public function having()
+	public function having($having, $mode = self::MODE_APPEND)
 	{
+		$having= is_array($having) ? implode(', ', $having) : $having;
+		$this->_section('having', $having, $mode);
 		return $this;
 	}
 
-	public function insert()
+	public function insert(array $fields)
 	{
+		$this->_sections['base'] = "INSERT INTO ".$this->_table.'('.implode(', ', array_keys($fields)).')';
+		$this->values(implode(', ', $fields));
+
 		$this->_setQueryType(self::TYPE_INSERT);
 		return $this;
 	}
 
-	public function join($table, $on, $join_type = self::JOINTYPE_INNER)
+	public function join($table, $on, $join_type = self::JOINTYPE_INNER, $mode = self::MODE_APPEND)
 	{
 		switch($join_type) {
+			case self::JOINTYPE_INNER:
+				$val = 'INNER JOIN ';
+				break;
+			case self::JOINTYPE_LOUTER:
+				$val = 'LEFT OUTER JOIN ';
+				break;
+			case self::JOINTYPE_ROUTER:
+				$val = 'RIGHT OUTER JOIN ';
+				break;
 		}
 
-		$this->_section('joins', $val, self::MODE_APPEND);
+		$val .= $table.' ON '.$on;
+
+		$this->_section('joins', $val, $mode);
 		return $this;
+	}
+
+	public function limit($limit, $offset = null)
+	{
+		$this->_section('limit', $limit);
+
+		if(!is_null($offset))
+			$this->offset($offset);
+	}
+
+	public function offset($offset)
+	{
+		$this->_section('offset', $offset);
 	}
 
 	public function select($fields = '*')
 	{
 		$fields = is_array($fields) ? implode(', ', $fields) : $fields;
 
-		$this->_section('base', 'SELECT '.$fields.' FROM '.$this->_table, self::MODE_OVERRIDE);
+		$this->_section('base', 'SELECT '.$fields.' FROM '.$this->_table);
 
 		$this->_setQueryType(self::TYPE_SELECT);
 		return $this;
 	}
 
-	public function set()
+	public function set(array $fields, $mode = self::MODE_APPEND)
 	{
+		$set = array();
+		foreach($fields as $k => $v)
+			$set[] = $k.'=\''.$v.'\'';
+
+		$this->_section('set', $set, $mode);
 	}
 
-	public function update()
+	public function update($fields)
 	{
 		$this->_sections['base'] = 'UPDATE '.$this->_table;
 
+		$this->set($fields, self::MODE_OVERRIDE);
 		$this->_setQueryType(self::TYPE_UPDATE);
 		return $this;
 	}
 
-	public function values()
+	public function values($values, $mode = self::MODE_OVERRIDE)
 	{
+		$values = is_array($values) ? implode(', ', $values) : $values;
+		$this->_section('values', $values, $mode);
 		return $this;
 	}
 
@@ -117,17 +158,25 @@ class Builder
 
 		switch ($this->_queryType) {
 			case self::TYPE_DELETE:
-				$query .= $this->_getSection('joins');
+				//$query .= $this->_getSection('joins');
 				$query .= $this->_getSection('where');
+				$query .= $this->_getSection('limit');
 				break;
 			case self::TYPE_INSERT:
+				$query .= $this->_getSection('values');
 				break;
 			case self::TYPE_SELECT:
 				$query .= $this->_getSection('joins');
 				$query .= $this->_getSection('where');
+				$query .= $this->_getSection('group');
+				$query .= $this->_getSection('having');
+				$query .= $this->_getSection('limit');
 				break;
 			case self::TYPE_UPDATE:
 				$query .= $this->_getSection('joins');
+				$query .= $this->_getSection('set');
+				$query .= $this->_getSection('where');
+				$query .= $this->_getSection('limit');
 				break;
 		}
 
@@ -136,27 +185,35 @@ class Builder
 
 	private function _getSection($section)
 	{
-		$ret = '';
-
 		if(!isset($this->_sections[$section])) 
-			return $ret;
+			return '';
+
+		$prepend_space = true;
+		$ret = false;
 
 		$val = $this->_sections[$section];
-		$ret .= ' ';
 		switch ($section) {
-			case 'delete':
-				break;
 			case 'group':
+				$ret .= 'GROUP BY '.(is_string($val) ? $val : implode(', ', $val));
 				break;
 			case 'having':
+				$ret .= 'HAVING '.(is_string($val) ? $val : implode(', ', $val));
 				break;
-			case 'insert':
+			case 'joins':
+				$ret .= is_string($val) ? $val : implode(' ', $val);
 				break;
-			case 'join':
+			case 'limit':
+				$ret .= 'LIMIT '.$val;
+				$ret .= $this->_getSection('offset');
 				break;
-			case 'select':
+			case 'offset':
+				$ret .= ', '.$val;
+				$prepend_space = false;
 				break;
 			case 'update':
+				break;
+			case 'set':
+				$ret .= 'SET '.(is_array($val) ? implode(', ', $val) : $val);
 				break;
 			case 'where':
 				$ret .= 'WHERE ';
@@ -168,20 +225,30 @@ class Builder
 				}
 				break;
 			case 'values':
+				$ret .= 'VALUES('.(is_array($val) ? implode(', ') : $val).')';
 				break;
 		}
 
-		return $ret;
+		if(!$ret) {
+			return '';
+		} else {
+			if($prepend_space)
+				$ret = ' '.$ret;
+
+			return $ret;
+		}
 	}
 
-	private function _section($section, $val, $mode)
+	private function _section($section, $val, $mode = self::MODE_OVERRIDE)
 	{
 		switch($mode) {
 			case self::MODE_OVERRIDE:
 				$this->_sections[$section] = $val;
 				break;
 			case self::MODE_APPEND:
-				if(is_array($this->_sections[$section])) {
+				if(!isset($this->_sections[$section])) {
+					$this->_sections[$section] = $val;
+				} elseif(is_array($this->_sections[$section])) {
 					$this->_sections[$section][] = $val;
 				} else {
 					$tmp = $this->_sections[$section];
