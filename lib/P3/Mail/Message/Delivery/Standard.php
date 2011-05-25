a3b76ee20442ddeafd70dd388feaa7b351eb793b
Tim Frazier <tim.frazier@gmail.com>
2011-05-25 11:13:23 -0500

    Need to inform user im too lazy to build my own SMTP suppport (for now), and to use PEAR

--- lib/P3/Mail/Message/Delivery/Standard.php
+++ lib/P3/Mail/Message/Delivery/Standard.php
@@ -11,6 +11,9 @@ class Standard implements iDelivers
 {
 	public function deliver($message)
 	{
+		if($message->flags & \P3\Mail\FLAG_SEND_USING_SMTP)
+			throw new \P3\Exception\MailMessageException("Sorry, but SMPT is not currently supported on P3s standard delivery interface.  Please see wiki for using PEAR w/ SMTP");
+
 		return mail($message->to, $message->subject, $message->body, $message->headers());
 	}
 }
