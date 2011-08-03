5926cc615853c0ad5064d9825c5b1603c4e8828f
Tim Frazier <tim.frazier@gmail.com>
2011-08-03 11:02:48 -0500

    Text year field should have had a maxlength attribute set to 4

--- lib/P3/Helper/helpers/form.php
+++ lib/P3/Helper/helpers/form.php
@@ -205,7 +205,7 @@ class form extends P3\Helper\Base
 						continue;
 					}
 
-					$ret .= $this->textField($field.'('.$parts[$item].'i)', array_merge(array('value' => $y, 'size' => 3)));
+					$ret .= $this->textField($field.'('.$parts[$item].'i)', array_merge(array('value' => $y, 'maxlength' => 4, 'size' => 3)));
 				break;
 				default:
 					switch($item) {
