aed773b62d311cb7b1eb6fc7053b5f4c629f2d13
Tim Frazier <tim.frazier@gmail.com>
2011-09-26 01:06:18 -0500

    Fixed bug in polymorphic get on null fks.  Also ridded old code causing error

--- lib/P3/ActiveRecord/Base.php
+++ lib/P3/ActiveRecord/Base.php
@@ -394,6 +394,9 @@ abstract class Base extends \P3\Model\Base
 			if(isset($belongsTo[$field]['fk']) && is_null($this->_data[$belongsTo[$field]['fk']])) {
 				return null;
 			} elseif(isset($belongsTo[$field]['polymorphic']) && $belongsTo[$field]['polymorphic']) {
+				if(in_array(null, array($this->_data[$field.'_id'], $this->_data[$field.'_type'])))
+					return null;
+
 				$belongsTo[$field]['polymorphic_as'] = $field;
 				return new Association\PolymorphicAssociation($this, $belongsTo[$field]);
 			}
@@ -1310,10 +1313,6 @@ abstract class Base extends \P3\Model\Base
 			if(!$assoc) 
 				return null;
 
-			if($this->isNew()) {
-				if(!is_a($assoc, 'P3\ActiveRecord\Association\BelongsToAssociation'))
-					throw new \P3\Exception\ActiveRecordException("You cannot access the children of an unsaved parent");
-			}
 
 			$ret =  $assoc->inSingleMode() ? $assoc->first() : $assoc;
 
