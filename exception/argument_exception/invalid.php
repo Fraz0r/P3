6fddeb7be5635ead6906dce2ff80756b6ceff364
Tim Frazier <tim.frazier@gmail.com>
2012-10-11 10:09:11 -0500

    new exception

---
+++ exception/argument_exception/invalid.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace P3\Exception\ArgumentException;
+
+/**
+ * Description of invalid
+ *
+ * @author Tim Frazier <tim.frazier at gmail.com>
+ */
+class Invalid extends \P3\Exception\ArgumentException
+{ }
+
+?>
