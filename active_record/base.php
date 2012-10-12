f20a4be45c0f790e070a827d139eddd787e34e1f
Tim Frazier <tim.frazier@gmail.com>
2012-10-12 17:22:33 -0500

    started work on active_record and collections

--- active_record/base.php
+++ active_record/base.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace P3\ActiveRecord;
+use P3\Builder\Sql as SqlBuilder;
 
 /**
  * Description of base
@@ -9,6 +10,67 @@ namespace P3\ActiveRecord;
  */
 abstract class Base extends \P3\Model\Base
 {
+	protected static $_pk = 'id';
+	protected static $_table;
+
+	public function __construct(array $data = [])
+	{
+		parent::__construct($data);
+	}
+
+//- Static
+	public static function all(array $options = [])
+	{
+		if(!isset($options['fields'])) {
+			$fields = '*';
+		} elseif(is_array($options['fields'])) {
+			$fields = implode(', ', $options['fields']);
+		} else {
+			$fields = $options['fields'];
+		}
+		
+		$builder = new SqlBuilder(static::get_table());
+		$builder->select($fields);
+
+		foreach($options as $k => $v)
+			$builder->{$k}($v);
+
+		return new Collection($builder, get_called_class());
+	}
+
+	public static function find($id_or_what, array $options_if_what = [])
+	{
+		if(is_numeric($id_or_what))
+			return self::all()->where([static::$_pk => $id_or_what])->limit(1)->fetch();
+
+		$ret = self::{$id_or_what}($options_if_what);
+
+		return $ret;
+	}
+
+	public static function first(array $options = [])
+	{
+		$ret = static::all();
+
+		foreach($options as $k => $v)
+			$ret->{$k}($v);
+
+		$ret->limit(1);
+
+		return $ret->fetch();
+	}
+
+	public static function get_table()
+	{
+		if(!empty(static::$_table))
+			return static::$_table;
+
+		$class = get_called_class();
+
+		static::$_table = \str::humanize(\str::pluralize($class));
+
+		return static::$_table;
+	}
 }
 
 ?>
--- active_record/collection.php
+++ active_record/collection.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace P3\ActiveRecord;
+use P3\Builder\Sql as SqlBuilder;
 
 /**
  * Description of collection
@@ -9,6 +10,159 @@ namespace P3\ActiveRecord;
  */
 class Collection  extends \P3\Object\Collection
 {
+	const STATE_STARTED = 1;
+	const STATE_FILLED  = 2;
+
+	private $_builder;
+	private $_count;
+	private $_state = 0;
+	private $_statement;
+
+	public function __construct(SqlBuilder $builder, $fetch_class = null)
+	{
+		$this->_builder = $builder;
+
+		if(!is_null($fetch_class))
+			$this->_builder->fetch_class($fetch_class);
+	}
+
+	public function average($what)
+	{
+		if($this->is_filled())
+			return parent::average($what);
+
+		$builder = clone $this->_builder;
+		$builder->fetch_class(null); //make sure we get an arr back
+		$builder->select("AVG({$what}) as avg", SqlBuilder::MODE_OVERRIDE);
+
+		$result = $builder->fetch();
+
+		return $result['avg'];
+	}
+
+	public function builder($builder = null)
+	{
+		return is_null($builder) ? $this->get_builder() : $this->set_builder($builder);
+	}
+
+	public function count()
+	{
+		if($this->is_filled())
+			return parent::count();
+
+		if(!isset($this->_count))
+			$this->_count = $this->_builder->fetch_count();
+
+		return $this->_count;
+	}
+
+	public function fetch()
+	{
+		if(!$this->is_started())
+			$this->_start();
+
+		//TODO:  Add dynamic flag
+		$this->_statement->setFetchMode(\PDO::FETCH_ASSOC);
+
+		$record = $this->_statement->fetch();
+
+		if($record && !is_null(($class = $this->_builder->fetch_class())))
+			$record = new $class($record);
+
+		$this->_data[$this->_pointer++] = $record;
+
+		if($record === FALSE)
+			$this->_state = $this->_state ^ self::STATE_FILLED;
+
+		return $record;
+	}
+
+	public function get_builder()
+	{
+		return $this->_builder;
+	}
+
+	public function is_filled()
+	{
+		if(!$this->is_started())
+			return false;
+
+		return (bool)$this->_state & self::STATE_FILLED;
+	}
+
+	public function is_started()
+	{
+		return (bool)$this->_state & self::STATE_STARTED;
+	}
+	
+	public function limit($limit, $offset = null)
+	{
+		$this->reset();
+
+		$this->_builder->limit($limit, $offset);
+
+		return $this;
+	}
+
+	public function max($what)
+	{
+		if($this->is_filled())
+			return parent::average($what);
+
+		$builder = clone $this->_builder;
+		$builder->fetch_class(null); //make sure we get an arr back
+		$builder->select("MAX({$what}) as max", SqlBuilder::MODE_OVERRIDE);
+
+		$result = $builder->fetch();
+
+		return $result['max'];
+	}
+
+	public function min($what)
+	{
+		if($this->is_filled())
+			return parent::average($what);
+
+		$builder = clone $this->_builder;
+		$builder->fetch_class(null); //make sure we get an arr back
+		$builder->select("MIN({$what}) as min", SqlBuilder::MODE_OVERRIDE);
+
+		$result = $builder->fetch();
+
+		return $result['min'];
+	}
+
+	public function reset()
+	{
+		$this->_data    = [];
+		$this->_pointer = 0;
+		$this->_count   = null;
+		$this->_state   = 0;
+		$this->_statement = null;
+	}
+
+	public function set_builder(SqlBuilder $builder)
+	{
+		$this->_builder = $builder;
+
+		return $this;
+	}
+
+	public function where($what, $mode = SqlBuilder::MODE_APPEND)
+	{
+		$this->reset();
+
+		$this->_builder->where($what, $mode);
+
+		return $this;
+	}
+
+//- Private
+	private function _start()
+	{
+		$this->_state = $this->_state ^ self::STATE_STARTED;
+		$this->_statement = $this->_builder->send();
+	}
 }
 
 ?>
