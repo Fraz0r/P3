c4cacc93c22b412e294ea21f4a3ecb0e4fd87487
Tim Frazier <tim.frazier@gmail.com>
2012-10-18 17:10:23 -0500

    added file info object from v1

---
+++ system/file_info.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace P3\System;
+
+/**
+ * This class extends SplFileInfo, handling inconsistencies between PHP versions
+ *
+ * @author Tim Frazier <tim.frazier at gmail.com>
+ * @package P3\System\Path
+ * @version $Id$
+ */
+class FileInfo extends \SplFileInfo
+{
+	/**
+	 * Return file extension (This wasn't added to the SPL Library until PHP 5.3.6)
+	 * If the method is there, it's used.  Otherwise we handle it on our own
+	 * 
+	 * @return string extension
+	 */
+	public function get_extension()
+	{
+		if (method_exists(get_parent_class($this), 'getExtension')) {
+			return(parent::getExtension());
+		} else {
+			return(substr($this->getFilename(), strrpos($this->getFilename(), '.')+1));
+		}
+	}
+
+	/**
+	 * Convenience method for getting the name of the file, minus the extension
+	 * 
+	 * @return string
+	 */
+	public function get_filename_no_extension()
+	{
+		return $this->getBasename('.'.$this->getExtension());
+	}
+}
