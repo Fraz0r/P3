e3db0e9ad246d97b8d4210c1397574c3cc66f21d
Tim Frazier <tim.frazier@gmail.com>
2012-10-25 13:31:54 -0500

    added new configs

--- config/defaults.php
+++ config/defaults.php
@@ -5,8 +5,14 @@ $config = \P3\Config\Handler::singleton();
 /* The default root directory for action views templates */
 $config->action_view->base_path = \P3\ROOT.'/app/views';
 
+/* The default delivery handler for action mailers */
+$config->action_mailer->delivery_handler = 'P3\Mail\Message\Delivery\Standard';
+
 /* Class used for reading/writing active records */
 $config->active_record->fixture_class = 'P3\ActiveRecord\Fixture\Database';
+
+/* SMTP settings, or false to fallback to sendmail */
+$config->mail->delivery->smtp = false;
 
 /* What format to assume if not supplied in URL */
 $config->routing->default_format = 'html';
