b68f4b4b4adb49aec84969863efd4a04d1c21988
Tim Frazier <tim.frazier@gmail.com>
2012-11-30 09:19:26 -0600

    added missing mail message exception

---
+++ exception/mail_message_exception.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace P3\Exception;
+
+/**
+ * Description of mail_message_exception
+ *
+ * @author Tim Frazier <tim.frazier at gmail.com>
+ */
+class MailMessageException extends Base
+{ }
+
+?>
